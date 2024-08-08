d09729d7370d8d93f4232ed4d272ecf07fc99d99
unknown <wadess@ept.sn>
2024-08-08 16:33:35 +0000

    Basic homepage and room single page done

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path ( "" , views.home , name = "home" ) , 
+    path ( "room/<str:room_id>", views.room , name = "room" ) ,
     
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import request 
 
+# rooms data : 
+rooms = [
+    {"id" : 1 , "name" : "Python web dev" } , 
+    {"id" : 2 , "name" : "React JS " } , 
+    {"id" : 3 , "name" : "Springboot lovers" } , 
+    {"id" : 4 , "name" : "Redux optimization" } , 
+]
+
 # Home page : 
 def home ( request ) : 
-    return render ( request , "base/home.html" )
+    
+    context = { "rooms" : rooms } 
+    return render ( request , "base/home.html" , context )
+
+# Single room page : 
+def room ( request , room_id ) :
+    room = None 
+
+    for room_item in rooms : 
+        if room_item["id"] == int (room_id )  : 
+            room = room_item
+
+    context = { "room" : room }
+     
+    return render ( request , "base/room.html" , context  )
