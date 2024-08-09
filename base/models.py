fd2ce8567db863f99a07d51599893f581e98e2b6
unknown <wadess@ept.sn>
2024-08-09 07:32:34 +0000

    Database setup

--- base/models.py
+++ base/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+# Topic : 
+class Topic ( models.Model ) : 
+    name = models.CharField( max_length = 50 ) 
+    
+    def __str__ ( self ) : 
+        return self.name 
+
+
+
+# Room : 
+class Room ( models.Model ) : 
+    host = models.ForeignKey ( User , on_delete = models.SET_NULL , null = True )
+    # participants = models.ForeignKey ( User , )
+    topic = models.ForeignKey ( Topic , on_delete = models.SET_NULL , null = True )  
+    name = models.CharField ( max_length = 75 ) 
+    description = models.TextField ( null = True , blank = True ) 
+    created = models.DateTimeField ( auto_now_add = True ) 
+    updated = models.DateTimeField ( auto_now = True ) 
+
+    def __str__ ( self ) : 
+        return self.name
+    
+
+
+# Message : 
+class Message ( models.Model ) : 
+    sender = models.ForeignKey ( User , on_delete = models.CASCADE ) 
+    room = models.ForeignKey ( Room , on_delete = models.CASCADE ) 
+    body = models.TextField ( ) 
+    created = models.DateTimeField ( auto_now_add = True ) 
+    updated = models.DateTimeField ( auto_now = True ) 
+
+    def __str__ ( self ) : 
+        return self.body[0 : 25] + "..." 
+
--- base/views.py
+++ base/views.py
@@ -1,28 +1,23 @@
 from django.shortcuts import render
 from django.http import request 
+from .models import Room 
 
-# rooms data : 
-rooms = [
-    {"id" : 1 , "name" : "Python web dev" } , 
-    {"id" : 2 , "name" : "React JS " } , 
-    {"id" : 3 , "name" : "Springboot lovers" } , 
-    {"id" : 4 , "name" : "Redux optimization" } , 
-]
+
 
 # Home page : 
 def home ( request ) : 
-    
+
+    rooms = Room.objects.all () 
     context = { "rooms" : rooms } 
+
     return render ( request , "base/home.html" , context )
 
 # Single room page : 
 def room ( request , room_id ) :
-    room = None 
+    
+    room = Room.objects.get ( id = room_id ) 
 
-    for room_item in rooms : 
-        if room_item["id"] == int (room_id )  : 
-            room = room_item
-
-    context = { "room" : room }
+    if room is not None : 
+        context = { "room" : room }
      
     return render ( request , "base/room.html" , context  )
